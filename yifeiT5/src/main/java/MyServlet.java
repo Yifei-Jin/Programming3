1f55b815d7ac9d837f21069aeb2fbbf622a2fb74
Yifei-Jin <yj819@ic.ac.uk>
2021-11-18 11:24:11 +0000

    New Tutorial 5

--- yifeiT5/src/main/java/MyServlet.java
+++ yifeiT5/src/main/java/MyServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
-            IOException{
+            IOException {
         resp.setContentType("text/html");
         resp.getWriter().write("Hello world!");
     }
